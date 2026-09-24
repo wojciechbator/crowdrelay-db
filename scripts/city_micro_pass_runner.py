63c7d5d8fda76862680f828e084e267d51b26f6c
Wojciech Bator <wojciech.jan.bator@gmail.com>
2026-09-24 11:35:51 +0200

    chore: bump city pass research version

--- scripts/city_micro_pass_runner.py
+++ scripts/city_micro_pass_runner.py
@@ -10,12 +10,12 @@ spec.loader.exec_module(module)
 
 # The canonical city key format is country::city. Keep the high-value city
 # revalidation list on the same key format used by city_key().
-module.PASS_FORMAT_VERSION = 6
+module.PASS_FORMAT_VERSION = 7
 module.FORCED_CITY_MIN_VERSION = {
-    "poland::bydgoszcz": 6,
-    "poland::warsaw": 6,
-    "poland::łódź": 6,
-    "germany::berlin": 6,
+    "poland::bydgoszcz": 7,
+    "poland::warsaw": 7,
+    "poland::łódź": 7,
+    "germany::berlin": 7,
 }
 
 module.main()
